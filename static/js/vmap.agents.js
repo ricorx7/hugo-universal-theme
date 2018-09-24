0ee1031d2e922222a19b289135d489af3e198505
Rico Castelo <rcastelo@rowetechinc.com>
2018-09-24 07:43:29 -0700

    Update agent map

--- static/js/vmap.agents.js
+++ static/js/vmap.agents.js
@@ -1,73 +1,74 @@
-
-  jQuery(document).ready(function () {
+jQuery(document).ready(function () {
     jQuery('#vmap').vectorMap({
-      map: 'world_en',
-      backgroundColor: '#ffffff',
-      color: '#ffffff',
-      hoverOpacity: 0.7,
-      selectedColor: '#666666',
-      enableZoom: true,
-      showTooltip: true,
-      scaleColors: ['#C8EEFF', '#006491'],
-      values: sample_data,
-      normalizeFunction: 'polynomial',
-      onRegionClick: function (event, code, region) {
-        switch (code) {
-          case "us":
-            window.location = "/agents_usa";
-          break;
-          case "br":
+        map: 'world_en',
+        backgroundColor: '#ffffff',
+        color: '#ffffff',
+        hoverOpacity: 0.7,
+        selectedColor: '#666666',
+        enableZoom: true,
+        showTooltip: true,
+        scaleColors: ['#C8EEFF', '#006491'],
+        values: sample_data,
+        normalizeFunction: 'polynomial',
+        onRegionClick: function (event, code, region) {
+            var agent_company = 'Rowe Technologies Inc';
+            var agent_title = 'USA - ' + agent_company;
+            var agent_name = "Rico Castelo";
+            var agent_addr1 = '11265 Danielson Ct, Suite 306';
+            var agent_addr2 = 'Poway, CA, 92064';
+            var agent_addr3 = 'USA';
+            var agent_phone = "858.842.3020";
+            var agent_fax = "858.842.3021";
+            var agent_website = "http://www.rowetechinc.com";
+            var agent_email = "sales@rowetechinc.com";
+            switch (code) 
+            {
+                case "us":
+                    window.location = "/agents_usa";
+                    break;
+                case "br":
+                    agent_company = 'Brazillian Company 1';
+                    agent_title = 'Brazil - ' + agent_company;
+                    agent_name = "Bob Joe";
+                    agent_addr1 = '1124 San Diego\n CA';
+                    agent_addr2 = '1124 San Diego\n CA';
+                    agent_addr3 = '1124 San Diego\n CA';
+                    agent_phone = "858.842.3020";
+                    agent_fax = "858.842.30202222";
+                    agent_email = "sales@rowetechinc.com";
+                    agent_website = "http:///www.test.com";
+                    break;
+                default:
+                    // Rowe Technologies is Default
+                    agent_company = 'Rowe Technologies Inc';
+                    agent_title = 'USA - ' + agent_company;
+                    agent_name = "Rico Castelo";
+                    agent_addr1 = '11265 Danielson Ct, Suite 306';
+                    agent_addr2 = 'Poway, CA, 92064';
+                    agent_addr3 = 'USA';
+                    agent_phone = "858.842.30201111";
+                    agent_fax = "858.842.3021";
+                    agent_email = "sales@rowetechinc.com";
+                    agent_website = "http://www.rowetechinc.com";
+                    break;
+            }
+            // Display the Modal Dialog of Agent Information
             $('#agentModal').on('show.bs.modal', function (event) {
-              var agent_company = 'Brazillian Company 1';
-              var agent_title = 'Brazil - ' + agent_company;
-              var agent_name = "Bob Joe";
-              var agent_addr1 = '1124 San Diego\n CA';
-              var agent_addr2 = '1124 San Diego\n CA';
-              var agent_addr3 = '1124 San Diego\n CA';
-              var agent_phone = "858.842.3020";
-              var agent_fax = "858.842.30202222";
-              var agent_email = "sales@rowetechinc.com"
-              var modal = $(this);
-              modal.find('.modal-title').text(agent_title)
-              modal.find('.agent_name').text(agent_name);
-              modal.find('.agent_company').text(agent_company);
-              modal.find('.agent_addr1').text(agent_addr1);
-              modal.find('.agent_addr2').text(agent_addr2);
-              modal.find('.agent_addr3').text(agent_addr3);
-              modal.find('.agent_phone').text(agent_phone);
-              modal.find('.agent_fax').text(agent_fax);
-              modal.find('.agent_email').text(agent_email);
-              modal.find('.agent_email').attr("href", "mailto:"+agent_email);
+                var modal = $(this);
+                modal.find('.modal-title').text(agent_title)
+                modal.find('.agent_name').text(agent_name);
+                modal.find('.agent_company').text(agent_company);
+                modal.find('.agent_addr1').text(agent_addr1);
+                modal.find('.agent_addr2').text(agent_addr2);
+                modal.find('.agent_addr3').text(agent_addr3);
+                modal.find('.agent_phone').text(agent_phone);
+                modal.find('.agent_fax').text(agent_fax);
+                modal.find('.agent_email').text(agent_email);
+                modal.find('.agent_email').attr("href", "mailto:" + agent_email);
+                modal.find('.agent_website').text(agent_website);
+                modal.find('.agent_website').attr("href", agent_website);
             });
-            break;
-          default:
-            // Rowe Technologies is Default
-            $('#agentModal').on('show.bs.modal', function (event) {
-              var agent_company = 'Rowe Technologies Inc';
-              var agent_title = 'USA - ' + agent_company;
-              var agent_name = "Rico Castelo";
-              var agent_addr1 = '11265 Danielson Ct, Suite 306';
-              var agent_addr2 = 'Poway, CA, 92064';
-              var agent_addr3 = 'USA';
-              var agent_phone = "858.842.30201111";
-              var agent_fax = "858.842.3021"
-              var agent_email = "sales@rowetechinc.com"
-              var modal = $(this);
-              modal.find('.modal-title').text(agent_title)
-              modal.find('.agent_name').text(agent_name);
-              modal.find('.agent_company').text(agent_company);
-              modal.find('.agent_addr1').text(agent_addr1);
-              modal.find('.agent_addr2').text(agent_addr2);
-              modal.find('.agent_addr3').text(agent_addr3);
-              modal.find('.agent_phone').text(agent_phone);
-              modal.find('.agent_fax').text(agent_fax);
-              modal.find('.agent_email').text(agent_email);
-              modal.find('.agent_email').attr("href", "mailto:"+agent_email);
-            });
-            break;
+            $('#agentModal').modal('show');
         }
-        // Display the Modal Dialog of Agent Information
-        $('#agentModal').modal('show');
-      }
     });
-  });
+});
--- static/js/vmap.agents.usa.js
+++ static/js/vmap.agents.usa.js
@@ -1,9 +1,11 @@
-
-  jQuery(document).ready(function () {
+jQuery(document).ready(function () {
     jQuery('#vmap_usa').vectorMap({
         map: 'usa_en',
         backgroundColor: null,
-        color: '#ffffff',
+        borderColor: '#333333',
+        borderOpacity: 0.05,
+        borderWidth: 1,
+        color: '#40739e',
         enableZoom: false,
         showTooltip: true,
         selectedColor: null,
@@ -21,60 +23,60 @@
             nj: '#00aabd',
             pa: '#00aabd',
         },
-      onRegionClick: function (event, code, region) {
-        switch (code) {
-            case "co":
+        onRegionClick: function (event, code, region) {
+            var agent_company = 'Rowe Technologies Inc';
+            var agent_title = 'USA - ' + agent_company;
+            var agent_name = "Rico Castelo";
+            var agent_addr1 = '11265 Danielson Ct, Suite 306';
+            var agent_addr2 = 'Poway, CA, 92064';
+            var agent_addr3 = 'USA';
+            var agent_phone = "858.842.3020";
+            var agent_fax = "858.842.3021";
+            var agent_website = "http://www.rowetechinc.com";
+            var agent_email = "sales@rowetechinc.com";
+            switch (code) {
+                case "co":
+                    agent_company = 'Brazillian Company 1';
+                    agent_title = 'Brazil - ' + agent_company;
+                    agent_name = "Bob Joe";
+                    agent_addr1 = '1124 San Diego\n CA';
+                    agent_addr2 = '1124 San Diego\n CA';
+                    agent_addr3 = '1124 San Diego\n CA';
+                    agent_phone = "858.842.3020";
+                    agent_fax = "858.842.3020";
+                    agent_website = "http://www.rowetechinc.com";
+                    agent_email = "sales@rowetechinc.com";
+                    break;
+                default:
+                    // Rowe Technologies is Default
+                    agent_company = 'Rowe Technologies Inc';
+                    agent_title = 'USA - ' + agent_company;
+                    agent_name = "Rico Castelo";
+                    agent_addr1 = '11265 Danielson Ct, Suite 306';
+                    agent_addr2 = 'Poway, CA, 92064';
+                    agent_addr3 = 'USA';
+                    agent_phone = "858.842.3020";
+                    agent_fax = "858.842.3021";
+                    agent_website = "http://www.rowetechinc.com";
+                    agent_email = "sales@rowetechinc.com";
+            }
+            // Display the Modal Dialog of Agent Information
             $('#agentModal').on('show.bs.modal', function (event) {
-              var agent_company = 'Brazillian Company 1';
-              var agent_title = 'Brazil - ' + agent_company;
-              var agent_name = "Bob Joe";
-              var agent_addr1 = '1124 San Diego\n CA';
-              var agent_addr2 = '1124 San Diego\n CA';
-              var agent_addr3 = '1124 San Diego\n CA';
-              var agent_phone = "858.842.3020";
-              var agent_fax = "858.842.3020";
-              var agent_email = "sales@rowetechinc.com"
-              var modal = $(this);
-              modal.find('.modal-title').text(agent_title)
-              modal.find('.agent_name').text(agent_name);
-              modal.find('.agent_company').text(agent_company);
-              modal.find('.agent_addr1').text(agent_addr1);
-              modal.find('.agent_addr2').text(agent_addr2);
-              modal.find('.agent_addr3').text(agent_addr3);
-              modal.find('.agent_phone').text(agent_phone);
-              modal.find('.agent_fax').text(agent_fax);
-              modal.find('.agent_email').text(agent_email);
-              modal.find('.agent_email').attr("href", "mailto:"+agent_email);
+                var modal = $(this);
+                modal.find('.modal-title').text(agent_title)
+                modal.find('.agent_name').text(agent_name);
+                modal.find('.agent_company').text(agent_company);
+                modal.find('.agent_addr1').text(agent_addr1);
+                modal.find('.agent_addr2').text(agent_addr2);
+                modal.find('.agent_addr3').text(agent_addr3);
+                modal.find('.agent_phone').text(agent_phone);
+                modal.find('.agent_fax').text(agent_fax);
+                modal.find('.agent_email').text(agent_email);
+                modal.find('.agent_email').attr("href", "mailto:" + agent_email);
+                modal.find('.agent_website').text(agent_website);
+                modal.find('.agent_website').attr("href", agent_website);
             });
-            break;
-            default:
-            // Rowe Technologies is Default
-            $('#agentModal').on('show.bs.modal', function (event) {
-              var agent_company = 'Rowe Technologies Inc';
-              var agent_title = 'USA - ' + agent_company;
-              var agent_name = "Rico Castelo";
-              var agent_addr1 = '11265 Danielson Ct, Suite 306';
-              var agent_addr2 = 'Poway, CA, 92064';
-              var agent_addr3 = 'USA';
-              var agent_phone = "858.842.3020";
-              var agent_fax = "858.842.3021"
-              var agent_email = "sales@rowetechinc.com"
-              var modal = $(this);
-              modal.find('.modal-title').text(agent_title)
-              modal.find('.agent_name').text(agent_name);
-              modal.find('.agent_company').text(agent_company);
-              modal.find('.agent_addr1').text(agent_addr1);
-              modal.find('.agent_addr2').text(agent_addr2);
-              modal.find('.agent_addr3').text(agent_addr3);
-              modal.find('.agent_phone').text(agent_phone);
-              modal.find('.agent_fax').text(agent_fax);
-              modal.find('.agent_email').text(agent_email);
-              modal.find('.agent_email').attr("href", "mailto:"+agent_email);
-            });
-            break;
+            $('#agentModal').modal('show');
         }
-        // Display the Modal Dialog of Agent Information
-        $('#agentModal').modal('show');
-      }
     });
-  });
+});
